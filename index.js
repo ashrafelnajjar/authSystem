862e77b97918c6c5d6e378d47d2e410f0a66d023
Ashraf <ashrafmo7amed700@gmail.com>
2026-09-24 06:54:23 +0300

    first commit

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-require('dotenv').config();
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const connectDB = require("./src/config/db")
-const notfound = require('./src/middlewares/notfound')
-const errorhandler = require('./src/middlewares/error')
-const routerauth = require("./src/routers/auth")
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const connectDB = require("./src/config/db");
+const notfound = require("./src/middlewares/notfound");
+const errorhandler = require("./src/middlewares/error");
+const routerauth = require("./src/routers/auth");
 
-const app = express()
+const app = express();
 app.use(express.json());
 
-
-app.use(notfound)
-app.use(errorhandler)
-app.use("api/auth" , routerauth)
-
-
+app.use(notfound);
+app.use(errorhandler);
+app.use("/api/auth", routerauth);
+app.use("/", (req, res) => {
+  res.status(200).json({ message: "success" });
+});
 
 const PORT = process.env.PORT || 5000;
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
-
