a1514a46032def26e6f5aa013255f54d47e8e6e6
Ashraf <ashrafmo7amed700@gmail.com>
2026-09-24 07:34:13 +0300

    first commit

--- src/validators/auth.js
+++ src/validators/auth.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const signupSchema = new Joi.object({
   name: Joi.string().required().max(30),
   email: Joi.string().email().required(),
-  password: Joi.string().email().required().max(30),
+  password: Joi.string().required().max(30),
 });
 
 const loginSchema = new Joi.object({
